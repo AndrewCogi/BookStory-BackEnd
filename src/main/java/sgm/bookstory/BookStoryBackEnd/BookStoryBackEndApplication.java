9ad1b3d94a8a3350e735133dfd580fdcbf8d33d0
CHOHYUNSIK <whgustlr0326@gmail.com>
2023-09-05 20:31:29 +0900

    test : update

--- src/main/java/sgm/bookstory/BookStoryBackEnd/BookStoryBackEndApplication.java
+++ src/main/java/sgm/bookstory/BookStoryBackEnd/BookStoryBackEndApplication.java
@@ -24,7 +24,7 @@ public class BookStoryBackEndApplication {
 
 	@GetMapping("/hello")
 	public String HelloWorld(){
-		return "Hello World";
+		return "Hello World - Orca0917";
 	}
 
 	@GetMapping("/")
