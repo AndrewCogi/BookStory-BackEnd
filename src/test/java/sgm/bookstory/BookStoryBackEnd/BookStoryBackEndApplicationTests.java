797432253ae3209bc643615aa5cc29b92455a956
CHOHYUNSIK <whgustlr0326@gmail.com>
2023-09-05 20:05:43 +0900

    initial commit

---
+++ src/test/java/sgm/bookstory/BookStoryBackEnd/BookStoryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package sgm.bookstory.BookStoryBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
